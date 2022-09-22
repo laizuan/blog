aa85fb8e462505bb8566b6b123b2c2f4f0b6b312
laizuan <1032299142@qq.com>
2022-09-22 15:11:57 +0800

    5

--- .algolia/scraper.sh
+++ .algolia/scraper.sh
@@ -6,4 +6,4 @@ docker run \
     -e APPLICATION_ID=$(printenv ALGOLIA_APPLICATION_ID) \
     -e API_KEY=$(printenv ALGOLIA_WRITE_API_KEY) \
     -e "CONFIG=$(cat config.json | jq -r tostring)" \
-    algolia/docsearch-scraper:v1.13.0
+    algolia/docsearch-scraper
